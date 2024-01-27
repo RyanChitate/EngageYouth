5821ebf70ceddd6390d68540dcb8b6111dfca65b
Ryan Nenyasha Chitate <yashryan16@gmail.com>
2024-01-27 21:44:06 +0200

    update
    Wording under the logo isnt positioned

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let slideIndex = 0;
-showSlides();
 
 function moveSlide(n) {
-  showSlides(slideIndex += n);
+  slideIndex += n;
+  showSlides();
 }
 
 function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  if (slideIndex >= slides.length) { slideIndex = 0 }
-  if (slideIndex < 0) { slideIndex = slides.length - 1 }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  const slides = document.querySelectorAll('.slide');
+  const maxIndex = slides.length - 1;
+
+  if (slideIndex < 0) {
+    slideIndex = maxIndex;
+  } else if (slideIndex > maxIndex) {
+    slideIndex = 0;
   }
-  slides[slideIndex].style.display = "block";
+
+  const slideWidth = slides[0].offsetWidth;
+  const slideContainer = document.querySelector('.slider');
+
+  slideContainer.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
 }
